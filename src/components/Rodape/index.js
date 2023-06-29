d34c9156b980f56a65b8f1d206edf7691b6d5b3a
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-06-29 14:40:39 -0300

    projeto concluido

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css";
+
+function Rodape() {
+	return (
+		<footer className={styles.rodape}>
+			<h3>Desenvolvido por Alura.</h3>
+		</footer>
+	);
+}
+
+export default Rodape;
